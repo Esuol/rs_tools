b81994700302f89abd2355aa25ab998e512818a5
berlinen <1292393108@qq.com>
2024-07-07 17:20:02 +0800

    feat: test mul_size_hints

--- src/size_hint.rs
+++ src/size_hint.rs
@@ -79,3 +79,10 @@ pub fn min(a: SizeHint, b: SizeHint) -> SizeHint {
     };
     (lower, upper)
 }
+
+#[test]
+fn mul_size_hints() {
+    assert_eq!(mul((3, Some(4)), (3, Some(4))), (9, Some(16)));
+    assert_eq!(mul((3, Some(4)), (usize::MAX, None)), (usize::MAX, None));
+    assert_eq!(mul((3, None), (0, Some(0))), (0, Some(0)));
+}
